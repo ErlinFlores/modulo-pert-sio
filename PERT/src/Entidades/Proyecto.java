9a3f03cf3f17e6c18247d4b9161302cba10a1cbe
manuel.lorenze@gmail.com <manuel.lorenze@gmail.com@da3c514b-2227-104a-09a9-46ddb600cb0a>
2011-03-02 18:50:58 +0000

    Cambios en la entidad Proyecto

--- PERT/src/Entidades/Proyecto.java
+++ PERT/src/Entidades/Proyecto.java
@@ -5,10 +5,35 @@
 
 package Entidades;
 
+import java.util.List;
+
 /**
  *
  * @author Usuario
  */
 public class Proyecto {
 
+    public Proyecto(String n, List<Tarea> lt){
+        nombre = n;
+        tareas = lt;
+    }
+
+    private String nombre;
+    private List<Tarea> tareas;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Tarea> getTareas() {
+        return tareas;
+    }
+
+    public void setTareas(List<Tarea> tareas) {
+        this.tareas = tareas;
+    }
 }
