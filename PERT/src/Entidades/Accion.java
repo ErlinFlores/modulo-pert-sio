f02ff33fe70f356660de180f4a673064de69c547
manuel.lorenze@gmail.com <manuel.lorenze@gmail.com@da3c514b-2227-104a-09a9-46ddb600cb0a>
2011-03-02 21:27:46 +0000

    Se creó la clase Accion (como enum)

---
+++ PERT/src/Entidades/Accion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Entidades;
+
+/**
+ *
+ * @author Usuario
+ */
+public enum Accion {
+    crear,
+    modificar
+}
