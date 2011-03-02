10b7c0199639e04a7800116ef05d590127604c65
manuel.lorenze@gmail.com <manuel.lorenze@gmail.com@da3c514b-2227-104a-09a9-46ddb600cb0a>
2011-03-02 07:11:16 +0000

    Modificación inicial de la clase Main. Introducción de los métodos principales.

--- PERT/src/pert/Main.java
+++ PERT/src/pert/Main.java
@@ -15,7 +15,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Main m = new Main();
+        m.inicializar("UY","es");
     }
 
+    public void inicializar(String paisIdioma, String lenguajeIdioma){
+        
+    }
 }
