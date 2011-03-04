e69774774070762dbac16c7a373eee74b0c1884a
manuel.lorenze@gmail.com <manuel.lorenze@gmail.com@da3c514b-2227-104a-09a9-46ddb600cb0a>
2011-03-04 12:56:02 +0000

    Cambios en el formInicio

--- PERT/src/Forms/FormInicio.java
+++ PERT/src/Forms/FormInicio.java
@@ -12,12 +12,9 @@
 package Forms;
 
 import Entidades.Proyecto;
-import java.awt.Component;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import javax.swing.DefaultListModel;
-import javax.swing.JList;
 
 /**
  *
@@ -30,35 +27,45 @@ public class FormInicio extends javax.swing.JFrame {
         lugarConfigurado = new Locale(paisIdioma, lenguajeIdioma);
         initComponents();
         setearEtiquetas();
-        lstProyectos = new JList(new DefaultListModel());
         obtenerProyectosAlmacenados();
     }
 
     private Locale lugarConfigurado;
     private List<Proyecto> listaDeProyectos;
 
+    /**
+     * Se setean las etiquetas de la pantalla.
+     */
     private void setearEtiquetas(){
-        this.setTitle("Módulo PERT"); // Manejo de idioma!!!
-        this.lblListaDeProyectos.setText("Lista de proyectos almacenados:"); // Manejo de idioma!!!
-        this.btnNuevo.setText("Nuevo"); // Manejo de idioma!!!
-        this.btnAbrir.setText("Abrir"); // Manejo de idioma!!!
-        this.btnBorrar.setText("Borrar"); // Manejo de idioma!!!
-        this.btnSalir.setText("Salir"); // Manejo de idioma!!!
+        setTitle("Módulo PERT"); // Manejo de idioma!!!
+        lblListaDeProyectos.setText("Lista de proyectos almacenados:"); // Manejo de idioma!!!
+        btnNuevo.setText("Nuevo"); // Manejo de idioma!!!
+        btnAbrir.setText("Abrir"); // Manejo de idioma!!!
+        btnBorrar.setText("Borrar"); // Manejo de idioma!!!
+        btnSalir.setText("Salir"); // Manejo de idioma!!!
     }
 
+    /**
+     * Se obtienen los proyectos que previamente han sido ingresados al sistema y almacenados en los archivos de persistencia.
+     */
     private void obtenerProyectosAlmacenados(){
         listaDeProyectos = new ArrayList<Proyecto>();
+
+        // Lógica de acceso a los archivos con proyectos almacenados!!!
+
         for(int i = 0; i < listaDeProyectos.size(); i++){
-            Proyecto p = listaDeProyectos.get(i);
-            lstProyectos.add(null, p); 
+            cmbListaDeProyectos.addItem(listaDeProyectos.get(i).getNombre());
         }
-        lstProyectos.updateUI();
-        // Lógica de acceso a los archivos.
+        cmbListaDeProyectos.updateUI();       
     }
 
+    /**
+     * Se agrega un proyecto creado recientemente a la lista comboBox de la pantalla.
+     * @param p
+     */
     public void agregarProyectoEnLista(Proyecto p){
-        //lstProyectos.add(p);
-        lstProyectos.updateUI();
+        cmbListaDeProyectos.addItem(p.getNombre());
+        cmbListaDeProyectos.updateUI();
     }
 
     /** This method is called from within the constructor to
@@ -73,10 +80,9 @@ public class FormInicio extends javax.swing.JFrame {
         btnNuevo = new javax.swing.JButton();
         btnAbrir = new javax.swing.JButton();
         btnBorrar = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        lstProyectos = new javax.swing.JList();
         lblListaDeProyectos = new javax.swing.JLabel();
         btnSalir = new javax.swing.JButton();
+        cmbListaDeProyectos = new javax.swing.JComboBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -101,9 +107,6 @@ public class FormInicio extends javax.swing.JFrame {
             }
         });
 
-        lstProyectos.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
-        jScrollPane1.setViewportView(lstProyectos);
-
         lblListaDeProyectos.setText("lblListadoDeProyectos");
 
         btnSalir.setText("btnSalir");
@@ -122,8 +125,8 @@ public class FormInicio extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(lblListaDeProyectos)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 264, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(cmbListaDeProyectos, 0, 252, Short.MAX_VALUE)
+                        .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(btnNuevo, javax.swing.GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE)
                             .addComponent(btnAbrir, javax.swing.GroupLayout.DEFAULT_SIZE, 80, Short.MAX_VALUE)
@@ -137,15 +140,14 @@ public class FormInicio extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(lblListaDeProyectos)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnNuevo)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnAbrir)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnBorrar))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnNuevo)
+                    .addComponent(cmbListaDeProyectos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnAbrir)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnBorrar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 81, Short.MAX_VALUE)
                 .addComponent(btnSalir)
                 .addContainerGap())
         );
@@ -159,15 +161,16 @@ public class FormInicio extends javax.swing.JFrame {
     }//GEN-LAST:event_btnNuevoActionPerformed
 
     private void btnAbrirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAbrirActionPerformed
-        Proyecto p = listaDeProyectos.get(lstProyectos.getSelectedIndex());
+        Proyecto p = listaDeProyectos.get(cmbListaDeProyectos.getSelectedIndex());
         FormProyecto fp = new FormProyecto(this, listaDeProyectos, p);
         fp.setVisible(true);
     }//GEN-LAST:event_btnAbrirActionPerformed
 
     private void btnBorrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBorrarActionPerformed
-        int i = lstProyectos.getSelectedIndex();
+        int i = cmbListaDeProyectos.getSelectedIndex();
+        cmbListaDeProyectos.removeItemAt(i);
+        cmbListaDeProyectos.updateUI();
         listaDeProyectos.remove(i);
-        lstProyectos.remove(i);
     }//GEN-LAST:event_btnBorrarActionPerformed
 
     private void btnSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalirActionPerformed
@@ -191,9 +194,8 @@ public class FormInicio extends javax.swing.JFrame {
     private javax.swing.JButton btnBorrar;
     private javax.swing.JButton btnNuevo;
     private javax.swing.JButton btnSalir;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JComboBox cmbListaDeProyectos;
     private javax.swing.JLabel lblListaDeProyectos;
-    private javax.swing.JList lstProyectos;
     // End of variables declaration//GEN-END:variables
 
 }
