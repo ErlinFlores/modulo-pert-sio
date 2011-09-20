5d3ddbd0f0092991b7eccfcc12035b0fa6a1b3cf
manuel.lorenze <manuel.lorenze@da3c514b-2227-104a-09a9-46ddb600cb0a>
2011-09-20 02:37:11 +0000

    Anda bastante bien. Quedan detalles de presentación y cálculo de duración

--- trunk/PERT/src/Entidades/TablaZeta.java
+++ trunk/PERT/src/Entidades/TablaZeta.java
@@ -40,20 +40,39 @@ public class TablaZeta {
         tablaZ = ManejadorDeArchivos.cargarTablaZ();
     }
     
-    public double obtenerProbabilidad(double zeta){
-        if ((-3.0 <= zeta) && (zeta <= 3.0)){
-            double zetaAbs = Math.abs(zeta);
-            int fila = (int)(zetaAbs*10);
-            int columna = (int)(Math.round(((zetaAbs*10) - fila)*10));
-            if (zeta >= 0){
-                return tablaZ[fila][columna];
-            }
-            return 1 - tablaZ[fila][columna];
-        }
-        return -1;
+    public double obtenerProbabilidad(double zetaAcotado){
+        if ((-3.09 <= zetaAcotado) && (zetaAcotado <= 3.09)){
+            double zetaAcotadoAbs = Math.abs(zetaAcotado);
+            int fila = (int)(zetaAcotadoAbs*10);
+            int columna = (int)(((zetaAcotadoAbs*10)-fila)*10);
+            double probabilidadExtendida;
+            if (zetaAcotado >= 0){
+                probabilidadExtendida = tablaZ[fila][columna];
+            }else{
+                probabilidadExtendida = 1 - tablaZ[fila][columna];
+            }            
+            return Math.round(probabilidadExtendida*10000)/10000.0;
+        }else if(zetaAcotado > 3.09){
+            return 1;
+        }else{
+            return 0;
+        }        
     }
     
     public double obtenerZeta(double probabilidad){
-        return 1.28;
+        double diferenciaAnterior = Math.abs(probabilidad - tablaZ[0][0]);
+        int filaAnterior = 0;
+        int columnaAnterior = 0;
+        for(int filaActual = 0; filaActual <= 30; filaActual++){
+            for(int columnaActual = 0; columnaActual <= 9; columnaActual++){
+                double diferenciaActual = Math.abs(probabilidad - tablaZ[filaActual][columnaActual]);
+                if ((diferenciaAnterior <= diferenciaActual) && !((filaActual == 0) && (columnaActual == 0))){
+                    return (filaAnterior/10.0)+(columnaAnterior/100.0);
+                }                
+                columnaAnterior = columnaActual;
+            }
+            filaAnterior = filaActual;
+        }
+        return (30/10.0)+(9/100.0);
     }
 }
