af132242d9dbc1da645ca07a1c1674ed5f4ccf1d
manuel.lorenze <manuel.lorenze@da3c514b-2227-104a-09a9-46ddb600cb0a>
2011-09-22 07:03:27 +0000

    .

---
+++ trunk/PERT/src/Demo/IDemo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Demo;
+
+import Entidades.Accion;
+import Entidades.RedDeTareas;
+
+/**
+ *
+ * @author Manuel Lorenze
+ */
+public interface IDemo {
+    
+    public String obtenerNombre();
+    
+    public String obtenerUnidadDeTiempo();
+    
+    public String obtenerDescripcion();    
+    
+    public RedDeTareas obtenerRedDeTareas();
+}
