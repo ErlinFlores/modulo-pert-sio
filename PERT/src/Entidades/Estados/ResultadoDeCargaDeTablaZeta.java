675b7bcc9a7905a9961ffbeac8e986abfff96475
manuel.lorenze <manuel.lorenze@da3c514b-2227-104a-09a9-46ddb600cb0a>
2011-09-27 03:39:07 +0000

    Revisión

---
+++ PERT/src/Entidades/Estados/ResultadoDeCargaDeTablaZeta.java
@@ -0,0 +1,16 @@
+package Entidades.Estados;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Manuel Lorenze
+ */
+public enum ResultadoDeCargaDeTablaZeta {
+    cargaExitosa,
+    archivoNoEncontrado,
+    datosInconsistentes
+}
