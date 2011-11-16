bf0936c3c4c1b62fea5d718eba617a741a3ed46a
manuel.lorenze <manuel.lorenze@da3c514b-2227-104a-09a9-46ddb600cb0a>
2011-11-16 17:20:09 +0000

    test

--- PERT/src/Demo/Demo3.java
+++ PERT/src/Demo/Demo3.java
@@ -7,9 +7,12 @@ package Demo;
 
 import Entidades.Estados.Accion;
 import Entidades.FabricaDeTareas;
+import Entidades.Precedencia;
 import Entidades.RedDeTareas;
 import Entidades.Tarea;
+import Entidades.TiempoEstimado;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -30,13 +33,37 @@ public class Demo3 implements IDemo{
         tipoAccion = Accion.modificar;
         nombre = "Proyecto Demo 3";
         unidadDeTiempo = "meses";
-        descripcion = "Ejemplo de un proyecto de 5 tareas.";        
+        descripcion = "Ejemplo de un proyecto de 4 tareas.";        
         redDeTareas = new RedDeTareas(new ArrayList<Tarea>());        
         agregarTareas();
     }
     
     private void agregarTareas(){
-        //Agregar tareas!!!
+        TiempoEstimado tiemposEstimados_A = new TiempoEstimado(1, 2, 9);
+        List<Tarea> tareasPrecedentes_A = new ArrayList<Tarea>();
+        Precedencia precedencia_A = new Precedencia(tareasPrecedentes_A);        
+        Tarea tarea_A = FabricaDeTareas.getInstance().crearTarea("TAREA A", tiemposEstimados_A, precedencia_A);
+        redDeTareas.agregarTarea(tarea_A);
+        
+        TiempoEstimado tiemposEstimados_B = new TiempoEstimado(2, 5, 20);
+        List<Tarea> tareasPrecedentes_B = new ArrayList<Tarea>();
+        Precedencia precedencia_B = new Precedencia(tareasPrecedentes_B);        
+        Tarea tarea_B = FabricaDeTareas.getInstance().crearTarea("TAREA B", tiemposEstimados_B, precedencia_B);
+        redDeTareas.agregarTarea(tarea_B);
+        
+        TiempoEstimado tiemposEstimados_C = new TiempoEstimado(4, 4, 40);
+        List<Tarea> tareasPrecedentes_C = new ArrayList<Tarea>();
+        tareasPrecedentes_C.add(tarea_A);
+        Precedencia precedencia_C = new Precedencia(tareasPrecedentes_C);        
+        Tarea tarea_C = FabricaDeTareas.getInstance().crearTarea("TAREA C", tiemposEstimados_C, precedencia_C);
+        redDeTareas.agregarTarea(tarea_C);
+        
+        TiempoEstimado tiemposEstimados_D = new TiempoEstimado(1, 5, 15);
+        List<Tarea> tareasPrecedentes_D = new ArrayList<Tarea>();
+        tareasPrecedentes_D.add(tarea_B);
+        Precedencia precedencia_D = new Precedencia(tareasPrecedentes_D);        
+        Tarea tarea_D = FabricaDeTareas.getInstance().crearTarea("TAREA D", tiemposEstimados_D, precedencia_D);
+        redDeTareas.agregarTarea(tarea_D);
     }
     
     public Accion obtenerTipoAccion() {
